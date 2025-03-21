64c802136a6a9042be7752292308186797038f4e
gauravv69 <gauravkadam2422@gmail.com>
2025-03-21 14:43:18 +0530

    Use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -34,24 +34,48 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size Chart
+      </a>
+    `;
+  }
+}
+const tshirt = new Clothing({
+  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  name: "Adults Plain Cotton T-Shirt - 2 Pack",
   rating: {
     stars: 4.5,
-    count: 87
+    count: 56
   },
-  priceCents: 1090,
+  priceCents: 799,
   keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
+    "tshirts",
+    "apparel",
+    "mens"
+  ],
+  type: "clothing",
+  sizeChartLink: "images/clothing-size-chart.png"
 });
-console.log(product1);
+console.log(tshirt);
+console.log(tshirt.getPrice());
 
 export const products = [
   {
@@ -713,6 +737,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-console.log(products);
